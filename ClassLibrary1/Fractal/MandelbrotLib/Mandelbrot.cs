be3597c5ffa1c6470b387da1491e69edc6d072da
Tuure Laurinolli <tuure@laurinolli.net>
2012-11-10 00:25:24 +0200

    Moved from TFS

---
+++ ClassLibrary1/Fractal/MandelbrotLib/Mandelbrot.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectFractalZoomer.Fractal.MandelbrotLib
+{
+    public interface Mandelbrot
+    {
+        /**
+         * Calculate mandelbrot fractal value at Mandelbrot space point (real, imag). The value returnes is in range [0,1] where 0 means definitely not part of Mandelbrot set and 1 means that the value had not diverged until end of iteration was reached.
+         */
+        float valueAt(float real, float imag);
+    }
+    public class BasicMandelbrot : Mandelbrot
+    {
+        private readonly int maxIters;
+        public BasicMandelbrot(int maxIters)
+        {
+            this.maxIters = maxIters;
+        }
+
+        public float valueAt(float r_0, float i_0)
+        {
+            float r = 0;
+            float i = 0;
+            for (int n = 0; n < maxIters; n++)
+            {
+                float r_temp = r * r - i * i + r_0;
+                i = 2 * r * i + i_0;
+                r = r_temp;
+                if (r * r + i * i > 2 * 2)
+                {
+                    return (float)n / maxIters;
+                }
+            }
+            return 1;
+        }
+    }
+}
---
+++ UnitTestLibrary1/Fractal/MandelbrotTest/MandelbrotTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using PerfectFractalZoomer.Fractal.MandelbrotLib;
+
+namespace PerfectFractalZoomer.Fractal.MandelbrotTest
+{
+    [TestClass]
+    public class BasicMandelbrotTest
+    {
+        [TestMethod]
+        public void TestValueInsideSet()
+        {
+            Mandelbrot m = new BasicMandelbrot(64);
+            Assert.AreEqual(1.0f, m.valueAt(0, 0));
+        }
+
+        [TestMethod]
+        public void TestValueOutsideSet()
+        {
+            Mandelbrot m = new BasicMandelbrot(64);
+            Assert.AreEqual(0.0f, m.valueAt(3, 0));
+        }
+    }
+}
